70cecc07708776e9c3cbbcd8459993e8f92026f3
PowerMobile Team <info@powermobile.org>
2018-08-19 04:18:16 +0200

    App update

---
+++ android/app/src/main/java/io/boostnote/MainApplication.java
@@ -0,0 +1,54 @@
+package io.boostnote;
+
+import android.app.Application;
+
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.soloader.SoLoader;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.microsoft.codepush.react.CodePush;
+import com.horcrux.svg.SvgPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+    @Override
+    protected String getJSBundleFile() {
+      return CodePush.getJSBundleFile();
+    }
+
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MyMainReactPackage(),
+          new RNDeviceInfo(),
+          new RNFetchBlobPackage(),
+          new CodePush("byLfL38UaCCfKxo_2kTLHuR190e81a2d16b3-a6fd-494f-8deb-050bb34a68be", MainApplication.this, BuildConfig.DEBUG),
+          new SvgPackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
